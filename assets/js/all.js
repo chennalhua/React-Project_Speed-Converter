81e58772fcf5438f4513735ab2ea0093333f2d82
Naihua <naihua.chen@mail.goldennet.com.tw>
2021-08-05 11:47:16 +0800

    [單位換算] 功能

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,10 +1,26 @@
+/*
+筆記
+    參數.toFixed(2) 取小數點幾位
+*/
+
 const App = () => {
+
+    //建立變數 inputNum
+    let [inputNum, setInputNum] = React.useState(0);
+
+    //監聽 換算 SET input value (雙向綁定)
+    const handleChange = (e) => {
+        let { value } = e.target;
+        //更新 inputNum
+        setInputNum(value);
+    }
+
     return (
         <div className="col-6 speed-box p-3">
             <header className="bg-blue text-light py-2 text-center">Network Speed Converter</header>
             <main className="container py-4">
                 <UnitTran />
-                <NumTran />
+                <NumTran handleChange={handleChange} inputNum={inputNum}/>
             </main>
             <footer>
                 <StateCard />
@@ -28,19 +44,24 @@ const UnitTran = () => {
 }
 
 //換算
-const NumTran = () => {
+const NumTran = (props) => {
+    //父層 App 傳進來的資料
+    const {handleChange} = props;
+    const {inputNum} = props;
+
     return (
         <div className="row text-center mt-4 align-items-center">
             <div className="col-5">
                 <label htmlFor="set" className="text-secondary fw-bolder mb-3">SET</label>
-                <input type="number" id="set" className="form-control" min="0" max="100"/>
+                <input type="number" id="set" className="form-control" min="0" max="100"
+                onChange={handleChange} value={inputNum}/>
             </div>
             <div className="col-2">
                 <i className="bi bi-arrow-right-short"></i>
             </div>
             <div className="col-5">
                 <p className="text-secondary fw-bolder">SHOW</p>
-                <p className="text-blue fw-bolder">0</p>
+                <p className="text-blue fw-bolder">{(inputNum / 8).toFixed(2)}</p> {/* Mbps換算MB/s要除8 */}
             </div>
         </div>
     )
