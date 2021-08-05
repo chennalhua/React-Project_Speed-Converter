6f167fab85ced2eb8f5578e41749dff378dba6b5
Naihua <naihua.chen@mail.goldennet.com.tw>
2021-08-05 11:23:13 +0800

    新增外觀 style

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,8 @@
 const App = () => {
     return (
-        <div>
-            <header>Network Speed Converter</header>
-            <main>
+        <div className="col-6 speed-box p-3">
+            <header className="bg-blue text-light py-2 text-center">Network Speed Converter</header>
+            <main className="container py-4">
                 <UnitTran />
                 <NumTran />
             </main>
@@ -17,22 +17,39 @@ const App = () => {
 //單位轉換
 const UnitTran = () => {
     return (
-        <p>單位轉換</p>
+        <div className="row text-center">
+            <div className="col-5">Mbps</div>
+            <div className="col-2">
+                <div className="tranIcon"><i className="bi bi-arrow-left-right"></i></div>
+            </div>
+            <div className="col-5">MB/s</div>
+        </div>
     )
 }
 
 //換算
 const NumTran = () => {
     return (
-        <p>換算</p>
+        <div className="row text-center mt-4 align-items-center">
+            <div className="col-5">
+                <label htmlFor="set" className="text-secondary fw-bolder mb-3">SET</label>
+                <input type="number" id="set" className="form-control" min="0" max="100"/>
+            </div>
+            <div className="col-2">
+                <i className="bi bi-arrow-right-short"></i>
+            </div>
+            <div className="col-5">
+                <p className="text-secondary fw-bolder">SHOW</p>
+                <p className="text-blue fw-bolder">0</p>
+            </div>
+        </div>
     )
 }
 
 //狀態卡
 const StateCard = (props) => {
-    // console.log(state)
     return (
-        <p>狀態卡</p>
+        <p className="showState text-center py-2 bg-blue text-light mb-0">---</p>
     )
 }
 
